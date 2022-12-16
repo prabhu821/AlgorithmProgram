a0f8b4bef6577c7eb4f632a78c28f87a9bbfc27f
rishiraj007 <rishiraj@mounttalent.com>
2022-12-16 20:34:03 +0530

    [Prabhu]: Insertion Sort

--- AlgorithmProgram/Program.cs
+++ AlgorithmProgram/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to Algorithm Programs");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("\nSelect option to execute \n1.Insertion Sort \n2.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        InsertionSort insertionSort = new InsertionSort();
+                        insertionSort.Sort();
+                        break;
+                    default:
+                        flag = false;
+                        break;
+                }
+            }
         }
     }
 }
